06ca84a8fb829d740e3e443cfcd76ab91dc62abe
ArkarMinSwin <106399241@student.swin.edu.au>
2026-05-20 09:50:09 +0800

    modify password with id

--- process.php
+++ process.php
@@ -11,7 +11,7 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
 
-        if($username == 'admin' && $password == 'password123'){
+        if($username == 'admin' && $password == 'password241'){
             $_SESSION['user'] = $username;
             header('Location:welcome.php');
         }else{
